0e1a2f597fbfde6c8b365046427b49215a9678f0
reeshkeed <girish.daloso@gmail.com>
2019-03-21 15:44:02 +0800

    React Router

--- resources/js/index.admin.js
+++ resources/js/index.admin.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => <h1>Home Page</h1>;
+const About = () => <h1>About Page</h1>;
 
 //JSX
-const App = props => <h1>Hello from admin</h1>;
+const App = props => (
+    <Router>
+        <>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+        </>
+    </Router>
+);
 
 if (document.querySelector('#root')) {
     ReactDOM.render(<App />, document.querySelector('#root'));
